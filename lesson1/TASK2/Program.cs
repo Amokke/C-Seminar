6215dff0183bccc9a89134b4720062c690badafc
Amokke <24.02.1985@mail.ru>
2022-12-17 15:15:17 +0700

    Task 1

---
+++ lesson1/TASK2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+
+if (a > b) {
+    Console.WriteLine($"Наибольшее число {a}");
+}
+else 
+    Console.WriteLine($"Наибольшее число {b}");
+
+

a2da6d70c0e817bab6c7bb4e79b1e2c243c8bb54
Amokke <24.02.1985@mail.ru>
2022-12-17 16:05:57 +0700

    TASK 6

---
+++ lesson1/TASK6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+
+int b = a%2;
+if (b == 0) {
+    Console.WriteLine($"Число {a} четное");
+}
+else
+    Console.WriteLine($"Число {a} не четное");

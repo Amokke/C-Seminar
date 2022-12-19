59d5916bb3cad774ecdfe51faa0da67298865eb6
Amokke <24.02.1985@mail.ru>
2022-12-19 22:34:10 +0700

    TASK13

---
+++ lesson2/TASK13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+
+int ParsFirstNumber(int a)
+{
+    int number = a/100;
+    int number2 = number%10;
+    return number2;
+}
+
+if (a > 99){
+    int result = ParsFirstNumber(a);
+    Console.WriteLine($"Третья цифра: {result}");
+}
+else
+    Console.WriteLine($"Третьей цифры нет");

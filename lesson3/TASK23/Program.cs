226bc341d8625929d0c8bc97ad6f81633263b5b6
Amokke <24.02.1985@mail.ru>
2022-12-28 21:46:34 +0700

    TASK23

---
+++ lesson3/TASK23/Program.cs
@@ -0,0 +1,28 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+    if(int.TryParse(Console.ReadLine(), out result))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Ввели не число. Повторите ввод!");
+    }
+}
+
+return result;
+}
+
+
+int number = GetNumber("Введите число");
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}

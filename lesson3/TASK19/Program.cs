01d0f7b5ac116101a9d3a8e5f002063f55532cc3
Amokke <24.02.1985@mail.ru>
2022-12-28 21:19:56 +0700

    TASK19

---
+++ lesson3/TASK19/Program.cs
@@ -0,0 +1,33 @@
+﻿string GetNumber(string message)
+{
+    int number = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out number) && number > 9999 && number < 100000)
+        {
+            
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введено не пятизначное число");
+        }
+    }
+    string result = number.ToString();
+    return result;
+}
+
+string testnumber = GetNumber("Введите пятизначное число.");
+int len = testnumber.Length;
+
+if (testnumber[0] == testnumber[4] && testnumber[1] == testnumber[3])
+{
+    Console.WriteLine($"Да");
+}
+else
+{
+    Console.WriteLine($"Нет");
+}

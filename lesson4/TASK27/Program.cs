f3bb2888910b9a9f098eb406a3b7891f5b04412a
Amokke <24.02.1985@mail.ru>
2023-01-16 21:54:21 +0700

    TASK27

---
+++ lesson4/TASK27/Program.cs
@@ -0,0 +1,46 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+    if(int.TryParse(Console.ReadLine(), out result))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Ввели не число. Повторите ввод!");
+    }
+}
+
+return result;
+}
+
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
+
+int number = GetNumber("Введите число");
+int len = NumberLen(number);
+SumNumbers(number, len);

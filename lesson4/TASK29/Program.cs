59536a14f0542a0b068613504011070e2d8cc343
Amokke <24.02.1985@mail.ru>
2023-01-16 23:09:32 +0700

    TASK29

---
+++ lesson4/TASK29/Program.cs
@@ -0,0 +1,24 @@
+﻿string GetString(string message)
+{
+    string result = "";
+    Console.WriteLine(message);
+    result = Console.ReadLine();
+    return result;
+}
+
+int[] Array(string data)
+{
+    var array = data.Split(',');
+    int[] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = int.Parse(array[i]);
+        Console.Write(array[i] + " ");
+    }
+    return result;
+}
+
+string data = GetString("Введите 8 чисел через запятую");
+Console.Write("Элименты введенного массива:");
+Array(data);
+

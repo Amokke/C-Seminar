40c4cbd12099c19f8eceac15664bb70adc95766f
Amokke <24.02.1985@mail.ru>
2023-01-16 21:44:41 +0700

    TASK25

---
+++ lesson4/TASK25/Program.cs
@@ -0,0 +1,34 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+    if(int.TryParse(Console.ReadLine(), out result))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Ввели не число. Повторите ввод!");
+    }
+}
+
+return result;
+}
+
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+int numberA = GetNumber("Введите число A");
+int numberB = GetNumber("Введите число B");
+ToDegree(numberA, numberB);

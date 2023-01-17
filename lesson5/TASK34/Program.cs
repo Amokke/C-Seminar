105d72c75dc83357b9e7011e062145bb1f531120
Amokke <24.02.1985@mail.ru>
2023-01-17 21:19:20 +0700

    TASK34

---
+++ lesson5/TASK34/Program.cs
@@ -0,0 +1,63 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+    if(int.TryParse(Console.ReadLine(), out result))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Ввели не число. Повторите ввод!");
+    }
+}
+
+return result;
+}
+
+
+int[] InitArray(int dimension)
+{
+    int[] result = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        result[i] = rnd.Next(100, 999);
+    }
+    return result;
+}
+
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+int CountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]%2 == 0)
+        count++;
+    }
+    return count;
+}
+
+
+int dimensin = GetNumber("Введите размерность массива");
+int[] arr = InitArray(dimensin);
+
+PrintArray(arr);
+int evenNumbers = CountEven(arr);
+Console.WriteLine(evenNumbers);

3393d1b52db721dd99c1af24632f5c337292806b
Amokke <24.02.1985@mail.ru>
2023-01-23 22:24:43 +0700

    TASK43

---
+++ lesson6/TASK43/Program.cs
@@ -0,0 +1,36 @@
+﻿double GetNumber(string message)
+{
+    double result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(double.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод!");
+        }
+    }
+    return result;
+}
+
+(double, double) MinMaxNumbers(double b1, double k1, double b2, double k2)
+{
+    double x = 0;
+    double y = 0;
+    x = (-b2 + b1) / (-k1 + k2);
+    y = k2 * x + b2;
+    return (x,y);
+}
+
+
+double b1 = GetNumber("Введите первую точку, первой прямой");
+double k1 = GetNumber("Введите вторую точку, первой прямой");
+double b2 = GetNumber("Введите первую точку, второй прямой");
+double k2 = GetNumber("Введите вторую точку, второй прямой");
+(double x, double y) = MinMaxNumbers(b1,k1,b2,k2);
+Console.WriteLine($"Линии пересекутся в точке [{x} ; {y}]");

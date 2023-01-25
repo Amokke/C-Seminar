119ff655e49eb511bf1ec4bc09172f86b9c27ae1
Amokke <24.02.1985@mail.ru>
2023-01-25 22:24:24 +0700

    TASK52

---
+++ lesson7/TASK52/Program.cs
@@ -0,0 +1,68 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+
+int[,] InitArray(int rows, int columns, int leftBound, int rightBound)
+{
+    int[,] result = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            result[i, j] = rnd.Next(leftBound, rightBound);
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ArithmeticMean(int[,] arr)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum += arr[i, j];
+        }
+        Console.Write($"{sum / arr.GetLength(0)} ");
+    }
+}
+
+int rows = GetNumber("Введите количество строк массива");
+int columns = GetNumber("Введите количество столбцов массива");
+int leftBound = GetNumber("Введите левую границу значений");
+int rightBound = GetNumber("Введите правую границу значений");
+
+int[,] arr = InitArray(rows, columns, leftBound, rightBound);
+PrintArray(arr);
+ArithmeticMean(arr);

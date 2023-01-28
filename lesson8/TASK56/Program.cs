97d1ba23538eb4a82da8d848412fe2757394b33f
Amokke <24.02.1985@mail.ru>
2023-01-28 20:09:20 +0700

    TASK56

---
+++ lesson8/TASK56/Program.cs
@@ -0,0 +1,78 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+int[,] InitArray(int rows, int columns, int leftBound, int rightBound)
+{
+    int[,] result = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            result[i, j] = rnd.Next(leftBound, rightBound);
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindRowMinSumElements(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка");
+}
+
+int rows = GetNumber("Введите размер массива");
+int columns = rows;
+int leftBound = GetNumber("Введите левую границу значений");
+int rightBound = GetNumber("Введите правую границу значений");
+
+int[,] arr = InitArray(rows, columns, leftBound, rightBound);
+
+PrintArray(arr);
+FindRowMinSumElements(arr);
+Console.WriteLine();

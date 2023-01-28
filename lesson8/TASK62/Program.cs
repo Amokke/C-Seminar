d5cba4b18a3ae2fcf1f626750424140422b60378
Amokke <24.02.1985@mail.ru>
2023-01-28 21:02:42 +0700

    TASK62

---
+++ lesson8/TASK62/Program.cs
@@ -0,0 +1,58 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void InitArraySpiral(int[,] array, int n, int leftBound)
+{
+    int i = 0, j = 0;
+    int value = leftBound;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { array[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+int rows = GetNumber("Введите размер массива");
+int columns = rows;
+int leftBound = GetNumber("Введите начальное значение спирали");
+int[,] array = new int[rows, columns];
+InitArraySpiral(array, rows, leftBound);
+
+PrintArray(array);
+Console.WriteLine();

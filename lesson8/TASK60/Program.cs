849f9af7091f51c6f7f21026f22398967d77cc2c
Amokke <24.02.1985@mail.ru>
2023-01-28 20:42:36 +0700

    TASK60

---
+++ lesson8/TASK60/Program.cs
@@ -0,0 +1,34 @@
+﻿void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                Console.Write($"{arr[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+void InitArray(int[,,] arr)
+{
+    int count = new Random().Next(10, 20);
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[k, i, j] += count;
+                count += 7;
+            }
+        }
+    }
+}
+
+int[,,] array3D = new int[2, 2, 2];
+InitArray(array3D);
+PrintIndex(array3D);

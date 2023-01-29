78af92980dad9a6fde3a68d363a8b97cb797ad0f
Amokke <24.02.1985@mail.ru>
2023-01-29 19:43:27 +0700

    TASK68

---
+++ lesson9/TASK68/Program.cs
@@ -0,0 +1,35 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int m = GetNumber("Введите число M");
+int n = GetNumber("Введите число N");
+
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");

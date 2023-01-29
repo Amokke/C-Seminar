ec8d29b95d6c76419d8eca8f924cea34adfc2a75
Amokke <24.02.1985@mail.ru>
2023-01-29 19:18:06 +0700

    TASK64

---
+++ lesson9/TASK64/Program.cs
@@ -0,0 +1,37 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+void NaturalToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
+
+int number = GetNumber("Введите число N");
+int count = 1;
+NaturalToLow(number, count);

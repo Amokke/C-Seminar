18767cc86dd8c7e603af96e8bb5ba84078463675
Amokke <24.02.1985@mail.ru>
2023-01-29 19:36:31 +0700

    TASK66

---
+++ lesson9/TASK66/Program.cs
@@ -0,0 +1,34 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+int NaturalSumMN(int m, int n)
+{
+    if (m == n)
+        return n;
+    else if (m > n)
+        return 0;
+    return n + NaturalSumMN(m, n - 1);
+}
+
+int m = GetNumber("Введите число M");
+int n = GetNumber("Введите число N");
+
+Console.WriteLine($"Сумма натуральных чисел в промежутке от {m} до {n} = {NaturalSumMN(m, n)}");
